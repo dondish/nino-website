b8524fddee6a73e2f8384d0a9cb6dcf1ce8c9ec1
dondish <odedshapira1@gmail.com>
2019-09-10 13:45:31 +0300

    improving the navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,20 +4,65 @@
 
 <style>
 	nav {
+		margin: 0;
+		display: block;
+		width: inherit;
+		height: 61px;
 		font: 'Overpass', sans-serif; /* I really really like this font :)*/
 		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
+		background-color: #db0485
+	}
+	ul {
+  		list-style-type: none;
+		height: 60px;
+		margin: 0;
+		display: inline-block;
+	}
+	li {
+		height: 40px;
+		margin: 0;
+		padding: 0 5px;
+		float: left;
+	}
+	li a {
+		width: fit-content;
+		display: block;
+		text-decoration: none;
+	}
+	img {
+		margin-top: 5px;
+		height: 50px;
+		width: 50px;
+		border-radius: 50%;
+		background-color: white;
+	}
+	.ulr {
+		float: right;
+	}
+	.ulr li { 
+		position: relative;
+		top: 10px;
+		border-right: 2px #db0485 solid;
+		background-color: white;
+		border-radius: 3%;
+		margin: 0 3px;
+	}
+	.ulr li a {
+		padding: 8px 10px;
+		text-align: center;
 	}
 </style>
 
 <nav>
 	<ul>
-		<li><a class:selected={!segment} href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
+		<li><a href="."><img src="nino.png" alt="Nino"/></a></li>
+	</ul>
+	<ul class="ulr">
+		<li><a class:selected={ !segment } href='.'>home</a></li>
+		<li><a class:selected={ segment === "about" } href='about'>about</a></li>
+		<li><a class:selected={ segment === "docs" } href='docs'>documentation</a></li>
+		<li><a class:selected={ segment === "about" } href='dashboard'>dashboard</a></li>
 	</ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,7 +33,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Nino - Discord Moderation Bot</title>
 </svelte:head>
 
 <h1>Great success!</h1>
