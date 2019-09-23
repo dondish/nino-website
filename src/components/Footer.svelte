e8321a787997696cf315e68f81d45884a39aa9b6
dondish <odedshapira1@gmail.com>
2019-09-23 15:52:14 +0300

    removed font-awesome.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -13,7 +13,7 @@ footer.dark {
 
 <footer class:dark={$darkmode}>
 <span>Nino© 2019-2020</span><br>
-<span>Made with <Icon style="line-height: 21px;" data={faHeart} label="Love"/> using Svelte</span><br>
+<span>Made with <Icon style="line-height: 21px;" data={heart} label="Love"/> using Svelte</span><br>
 <span>Nino is not in any way affiliated with Discord Inc. Nakano Nino is a property of Kodansha Ltd.</span><br>
 <span style="line-height: 31px;">Dark Mode: <Switch bind:checked={$darkmode} /></span>
 </footer>
@@ -21,6 +21,6 @@ footer.dark {
 <script>
 import Switch from './switch.svelte'
 import Icon from 'svelte-awesome/components/Icon.svelte' 
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { heart } from 'svelte-awesome/icons'
 export let darkmode;
 </script>
